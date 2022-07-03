582097bce191cbb112b730e9b9a63870ccd8069a
Kashaedov Vitaliy <Vitalickashaedov@mail.ru>
2022-07-03 21:38:21 +0300

    zadacha13

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+string number1 = Console.ReadLine();
+thrichislo (number1);
+void  thrichislo (string number1 )
+{
+    if (number1.Length > 2)
+    {
+        Console.WriteLine ($" Третья цифра введенного Вами числа равна: {number1 [2]}");
+
+    }
+    else Console.WriteLine ($"В введенном Вами числе {number1} нет третьей цифры");
+
+
+   
+
+
+}
